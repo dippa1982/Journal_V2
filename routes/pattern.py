cbbf01ce182e201e9d265f3d43acb613f740bd52
Simon Brown <bpalpha.brown@googlemail.com>
2026-09-25 08:19:28 +0100

    updated version

--- routes/pattern.py
+++ routes/pattern.py
@@ -20,26 +20,7 @@ def patterns():
 
     emotion_history = build_emotion_history()
 
-    #intelligence = build_intelligence(current_user)
-
-    intelligence = {
-    "patterns": [
-        {
-            "type": "repeated_emotional_association",
-            "trigger": "Nicola's explanation of the Roy incident, including her statement about someone grabbing her",
-            "emotion": "anger",
-            "entry_ids": [48, 50],
-            "entry_count": 2
-        },
-        {
-            "type": "repeated_emotional_association",
-            "trigger": "Nicola's explanation of the Roy incident, including her statement about someone grabbing her",
-            "emotion": "distress",
-            "entry_ids": [48, 50],
-            "entry_count": 2
-        }
-    ]
-}
+    intelligence = build_intelligence(current_user)
 
     return render_template(
         "patterns.html",
